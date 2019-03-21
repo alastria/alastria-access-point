8de332dde071882955bdd5a5d2352c72a3a65d19
andresalo <andresalgr@gmail.com>
2019-03-21 18:07:28 +0100

    Update new conf path

--- allow-ip.sh
+++ allow-ip.sh
@@ -19,12 +19,12 @@ if [ $1 = "nginx" ]; then
         if [ $2 = "-r" ]; then
                 for IP in "${@:3}"
                 do
-                        docker exec $DOCKER_ID bash -c "sed -i '/$IP/d' /etc/nginx/whitelist"
+                        sed -i '/$IP/d' ./nginx/conf.d/whitelist
                 done
         else
                 for IP in "${@:2}"
                 do
-                        docker exec $DOCKER_ID bash -c "echo \"allow $IP;\" >> /etc/nginx/whitelist"
+                        echo "allow $IP;" >> ./nginx/conf.d/whitelist
                 done
         fi
         docker exec "$DOCKER_ID" nginx -s reload
